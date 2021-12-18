58efb9e631b6fca36ab19b505f40c97bbf3b1b18
Administrator <Administrator@DESKTOP-LO5FOLT>
2021-12-19 00:04:23 +0300

    Add project files.

---
+++ Yigin/Programs.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BTKALGORITMASI_NEW.Yigin
+{
+    public  class Programs
+    {
+        static void Main(string args)
+        {
+            Console.WriteLine("ömer doğu");
+
+        }
+
+    }
+}
